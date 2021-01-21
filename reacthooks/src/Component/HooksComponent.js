18be59a54bc61921ccd000106801dd546be227ac
aakashdeveloper <ahanda205@gmail.com>
2021-01-21 07:43:39 +0530

    a

---
+++ reacthooks/src/Component/HooksComponent.js
@@ -0,0 +1,39 @@
+import React,{useState,useEffect} from 'react';
+import DisplayComponent from './DisplayComponent';
+
+const url = "https://developerfunnel.herokuapp.com/location"
+
+
+function HooksComponent(){
+    const [title] = useState('Hooks App');
+    const [count,setCount] = useState(0)
+    const [count1,setCount1] = useState(0);
+    const [city,setCity] = useState()
+
+    const mycount  =() => {
+        setCount(count+1)
+    }
+
+    useEffect(() => {
+        fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            setCity(data)
+        })
+    })
+    return(
+        <div>
+            <center>
+                <h2>{title}</h2>
+                <h2>{count}</h2>
+                <button onClick={mycount}>Counter</button>
+                <h2>{count1}</h2>
+                <button onClick={() => {setCount1(count1+1)}}>Counter</button>
+                <DisplayComponent mycity={city}/>
+            </center>
+        </div>
+    )
+}
+
+
+export default HooksComponent
